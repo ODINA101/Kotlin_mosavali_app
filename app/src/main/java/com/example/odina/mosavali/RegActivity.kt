170c716121d425d6cd3067dce4d1844915b9adea
ODINA101 <iraklisamniashviliii@gmail.com>
2018-03-09 22:15:38 +0400

    updated

--- app/src/main/java/com/example/odina/mosavali/RegActivity.kt
+++ app/src/main/java/com/example/odina/mosavali/RegActivity.kt
@@ -1,16 +1,28 @@
 package com.example.odina.mosavali
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
 import android.widget.ImageButton
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.android.synthetic.main.activity_reg.*
 import java.util.concurrent.TimeUnit
 import javax.security.auth.callback.Callback
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.database.FirebaseDatabase
+
 
 class RegActivity : AppCompatActivity() {
 
@@ -23,46 +35,46 @@ class RegActivity : AppCompatActivity() {
             onBackPressed()
         })
         dastruribtn.setOnClickListener({
+           var mAuth = FirebaseAuth.getInstance()
 
-            startPhoneNumberVerification("+995597918113")
+            mAuth.signInAnonymously()
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            val user = mAuth.currentUser
 
+                            if (user != null) {
+                              var map = HashMap<String,String>()
+                                map["phone"] = reg_number.text.toString()
+                                map["name"] = reg_name.text.toString()
+                                map["region"] = raioni.text.toString()
+                                map["password"] = paroli.text.toString()
+                                map["uid"] = user.uid
+
+                                FirebaseDatabase.getInstance().getReference("Users").child(user.uid).setValue(map).addOnCompleteListener({
+                                    var home = Intent(this,homeActivity::class.java)
+                                    startActivity(home)
+                                })
+
+
+
+                            }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this@RegActivity, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show()
+                        }
+
+                        // ...
+                    }
 
         })
 
     }
-    private fun startPhoneNumberVerification(phoneNumber: String) {
-        var mCallbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
-        mCallbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-
-            }
-
-            override fun onVerificationFailed(e: FirebaseException) {
-                // This callback is invoked if an invalid request for verification is made,
-                // for instance if the the phone number format is invalid.
 
 
-                if (e is FirebaseAuthInvalidCredentialsException) {
-                    // Invalid request
-                } else if (e is FirebaseTooManyRequestsException) {
-                    // The SMS quota for the project has been exceeded
-                }
 
-            }
-            override fun onCodeSent(verificationId: String?,
-                                    token: PhoneAuthProvider.ForceResendingToken?) {
-                // The SMS verification code has been sent to the provided phone number, we
-                // now need to ask the user to enter the code and then construct a credential
-                // by combining the code with a verification ID.
-                // Save verification ID and resending token so we can use them later
 
-            }
-        }
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                phoneNumber, // Phone number to verify
-                60,             // Timeout duration
-                TimeUnit.SECONDS,   // Unit of timeout
-                this,           // Activity (for callback binding)
-                mCallbacks)        // OnVerificationStateChangedCallbacks
-    }
+
 }
---
+++ app/src/main/java/com/example/odina/mosavali/homeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.odina.mosavali
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class homeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+    }
+}
